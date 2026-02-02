8f671e49b4de666d8753ae2d509f982d0a217cff
Yonah Bergsma <y.bergsma14@gmail.com>
2026-02-02 20:29:05 +0100

    feat: add volatility KPI

--- app/api/analyze.py
+++ app/api/analyze.py
@@ -1,6 +1,7 @@
 from fastapi import APIRouter, HTTPException
 from app.services.data.market_data import get_historical_prices
 from app.services.kpis.returns import calculate_returns
+from app.services.kpis.volatility import calculate_volatility
 
 router = APIRouter()
 
@@ -9,10 +10,12 @@ def analyze(ticker:str):
     try:
         prices = get_historical_prices(ticker)
         returns = calculate_returns(prices)
+        volatility = calculate_volatility(prices)
     
         return{
             "ticker":ticker,
             "returns":returns,
+            "volatility":volatility,
         }
     
     except Exception as e:
